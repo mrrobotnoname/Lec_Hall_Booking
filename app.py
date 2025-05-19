657ec18fb5c8f3fa9ba827cbc23ca3dd0eb3cd7d
mrrobotnoname <dilabnoname1245@gmail.com>
2025-05-19 06:07:28 +0530

    admin is done

--- app.py
+++ app.py
@@ -24,20 +24,17 @@ class User(db.Model):
     username = db.Column(db.String(50), unique = True, nullable = False)
     password = db.Column(db.String(100), unique = True, nullable = False)
     user_level = db.Column(db.Integer, nullable = False)##Lectures are 1 and admin is 0 
-    department_id = db.Column(db.Integer,db.ForeignKey('department.id'),nullable = False)
 
 class Department(db.Model):
    id=db.Column(db.Integer,primary_key=True)
    department_name=db.Column(db.String(50),nullable=False,unique = True)
-   users = db.relationship('User',backref='department',lazy=True)
-   years = db.relationship('Year',backref='dm_year',lazy=True )
+   year = db.relationship('Year',backref='department',lazy=True )
 
 class Year(db.Model):
    id = db.Column(db.Integer,primary_key=True)
-   year = db.Column(db.Integer,nullable=False,unique=True)
    department_id = db.Column(db.Integer,db.ForeignKey('department.id'),nullable = False)
+   year = db.Column(db.Integer,nullable=False)
 
-   __table_args__ = (db.UniqueConstraint('department_id', 'year', name='_department_year_uc'),)
 
 class Hall(db.Model):
    id = db.Column(db.Integer,primary_key = True)
@@ -76,12 +73,10 @@ def login():
             if qurrey.user_level == 0:
                 session['user'] = qurrey.username
                 session['user_level'] = qurrey.user_level
-                session['department'] = qurrey.department.department_name
                 return redirect(url_for("admin"))
             if qurrey.user_level == 1:
                 session['user'] = qurrey.username
                 session ['user_level'] = qurrey.user_level
-                session['department'] = qurrey.department.department_name
                 return redirect(url_for('root'))
         flash("wrong password")
         return redirect(url_for('login'))
@@ -102,23 +97,47 @@ def admin():
       
       if request.method == "POST":
          value = request.form.get('action')
+
          if value == "add_department":
             department_name = request.form["name"]
             if Department.query.filter_by(department_name = department_name).one_or_none() is None:
                add = Department(department_name = department_name)
                db.session.add(add)
                db.session.commit()
-               flash("The department has been added",category="message")
+               flash("The department has been added",category="success")
                return redirect(url_for('admin'))
             flash("The Department is allrady exsist!",category="warning")
             return redirect(url_for("admin"))
+         
          if value == "delete_department":
             delValue= db.session.get(Department,int(request.form.get("department")))
             db.session.delete(delValue)
             db.session.commit()
-            flash("Departmet deleted sucsessfully!",category="message")
+            flash("Departmet deleted sucsessfully!",category="success")
             return redirect(url_for('admin'))
+         
+         if value == "add_year":
+            year = int(request.form['year'])
+            department = int(request.form.get("department"))
 
+            if Year.query.filter_by(department_id = department,year = year ).one_or_none() is None:
+               add = Year(department_id = department,year=year)
+               db.session.add(add)
+               db.session.commit()
+               flash("The year was added succsefully!",category="success")
+               return redirect(url_for("admin"))
+            flash("The year is allredy added!",category="warning")
+            return redirect(url_for("admin"))
+
+         if value == 'delete_year':
+            department = request.form.get('faculty')  
+            year = request.form.get('year')
+            deletes = Year.query.filter_by(department_id = int(department),year = int(year)).all()
+            for delete in deletes:   
+               db.session.delete(delete)
+            db.session.commit()
+            flash("The year was deleted successfully!",category='success')
+            return redirect(url_for("admin"))     
          if value == "add_hall":
             hallName = request.form["name"]
             hallType = request.form.get('hallType')
@@ -126,15 +145,58 @@ def admin():
                add = Hall(hallName = hallName,hallType=hallType,status = 0)
                db.session.add(add)
                db.session.commit()
+               flash("Hall been Added!",category="success")
                return redirect(url_for('admin'))
-            flash("Hall alrady exsist")
+            flash("Hall alrady exsist",category="warning")
             return redirect(url_for('admin'))         
          if value == "delete_hall":
-            deleteHall = request.form.get("faculty")
-      return render_template("admin.html",users=users,departments=departments[1:],halls=halls)
+            delValue = db.session.get(Hall,request.form.get("deleteHall"))
+            db.session.delete(delValue)
+            db.session.commit()
+            flash("The hall was deleted succsessfully!",category="success")
+            return redirect(url_for('admin'))
+         if value == "add_lecture":
+            name = request.form["name"]
+            username = request.form["username"]
+            password = request.form["password"]
+            if User.query.filter_by(username=username).one_or_none() is None:
+               add = User(name = name,username=username,password = password,user_level=1)
+               db.session.add(add)
+               db.session.commit()
+               flash("Lecture added successfully!",category="success")
+               return redirect(url_for('admin'))
+            flash("The lecture is already exsist!",category="warning")
+            return redirect(url_for('admin'))
+      return render_template("admin.html",users=users,departments=departments,halls=halls)
    return redirect(url_for('root'))
    
-   
+@app.route('/api')
+def api():
+   if request.args.get('action') is None:
+      return 'No action specified', 400
+   if request.args['action'] == 'getYears':
+      years = Year.query.filter_by(department_id = int(request.args['faculty'])).all()
+      return [year.year for year in years]     
+
+@app.route('/bookings',methods = ['GET', 'POST'])
+def bookings():
+   if 'user' not in session and session.get('user') is None:
+      return redirect(url_for('login'))
+   elif session.get('user_level',1) != 1:
+      abort(401)
+   else:
+      if request.method == "POST":
+         hall = request.form.get('hall')
+         year = request.form.get('year')
+         start_time = request.form.get('start_time')
+         end_time = request.form.get('end_time')
+         booking_session = str(datetime.now())
+         add = Booking(booking_session=booking_session,user_id=session['user'],hall_id=hall,year_id=year,start_time=start_time,end_time=end_time)
+         db.session.add(add)
+         db.session.commit()
+         flash("Booking has been added successfully!",category="success")
+         return redirect(url_for("bookings"))
+      return render_template("bookings.html")
 @app.route('/logout')
 def logout():
    session.pop('user',None)
@@ -142,16 +204,6 @@ def logout():
    session.pop('department',None)
    return redirect(url_for("root"))  
 
-
-@app.route('/lecture',methods=['GET','POST'])
-def lecture():
-   if 'user' not in session and session.get('user') is None:
-      return redirect(url_for('login'))
-   
-   datas = Hall.query.filter_by(status = False).all()
-   users = User.query.filter_by(username = session['user'])
-   print(users)
-   return render_template("lecture.html",datas=datas)
 
 @app.route('/install',methods = ['GET', 'POST'])
 def install():
@@ -166,12 +218,9 @@ def install():
          username = request.form['username']
          password = request.form['password']
 
-         add_department = Department(department_name = 'admin')
-         db.session.add(add_department)  
          db.session.commit()
          adduser = User(name = 'admin',username = username,
-                        password = password,user_level = 0,
-                        department_id = add_department.id)
+                        password = password,user_level = 0)
          db.session.add(adduser)
          db.session.commit()
 
